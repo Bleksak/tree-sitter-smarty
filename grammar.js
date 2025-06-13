2c3f1e0ed5498aa51eca60128d91513df01301a3
Jiri Velek <bleksak@protonmail.com>
2025-06-13 20:44:26 +0200

    try aliasing regex

--- grammar.js
+++ grammar.js
@@ -57,7 +57,7 @@ module.exports = grammar({
 
     foreach: $ => seq(
       '{foreach',
-      field('iterable', $.text),
+      field('iterable', alias(/[^}]+/, $.text)),
       // /\$[^\s]+/,
       'as',
       // field('key', $.php),
