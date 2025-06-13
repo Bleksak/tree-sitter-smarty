3f424a0c93684b3360745375d71c6c68e2955c4e
Jiri Velek <bleksak@protonmail.com>
2025-06-13 21:50:46 +0200

    add first last sep :D

--- grammar.js
+++ grammar.js
@@ -16,6 +16,9 @@ module.exports = grammar({
       $.text,
       $.foreach,
       $.if,
+      $.first,
+      $.last,
+      $.sep,
       // $.literal,
     ),
 
@@ -25,6 +28,9 @@ module.exports = grammar({
       $.text,
       $.foreach,
       $.if,
+      $.first,
+      $.last,
+      $.sep,
     ),
 
     comment: $ => seq('{*', /[^*]*/, '*}'),
